8cabd53e9a08f72f152389dcae939ff2b90a2ceb
Jentezar <104159657+Jentezar@users.noreply.github.com>
2025-02-19 08:08:51 -0500

    controller func

--- src/main/java/edu/iu/p466/prime_service/controller/PrimeController.java
+++ src/main/java/edu/iu/p466/prime_service/controller/PrimeController.java
@@ -1,4 +1,20 @@
 package edu.iu.p466.prime_service.controller;
 
+import edu.iu.p466.prime_service.service.IPrimeService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
 public class PrimeController {
+    IPrimeService primesService;
+    public PrimeController(IPrimeService primesService)
+    {
+        this.primesService = primesService;
+    }
+
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable int n) {
+        return primesService.isPrime(n);
+    }
 }
---
+++ src/test/java/edu/iu/p466/prime_service/service/PrimeServiceTest.java
@@ -0,0 +1,26 @@
+package edu.iu.p466.prime_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeServiceTest {
+    PrimeService primeService = new PrimeService();
+    @Test
+    void _45IsNotPrime() {
+        int n = 45;
+        boolean expected = false;
+        IPrimeService primesService;
+        boolean actual = primeService.isPrime(n);
+        assertEquals(expected, actual);
+
+    }
+
+    void _285191IsPrime() {
+        long n = 285191;
+        boolean expected = true;
+        boolean actual = primeService.isPrime(n);
+        assertEquals(expected, actual);
+    }
+
+}
