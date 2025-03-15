9f98179f82c6cd106ed1fc56fef4678b4ba4570d
Jentezar <104159657+Jentezar@users.noreply.github.com>
2025-03-15 10:37:16 -0400

    commit

--- src/main/java/edu/iu/p466/prime_service/config/CorsConfig.java
+++ src/main/java/edu/iu/p466/prime_service/config/CorsConfig.java
@@ -9,7 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Add your frontend URL here
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5500"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
