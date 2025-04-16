144a04790971c76818ebcbca5c6bad600a5f035f
Jentezar <104159657+Jentezar@users.noreply.github.com>
2025-04-16 12:45:22 -0400

    praac 8 grading

--- scripts/push-to-acr.sh
+++ scripts/push-to-acr.sh
@@ -4,6 +4,6 @@ set -u
 : "$CONTAINER_REGISTRY_UN"
 : "$CONTAINER_REGISTRY_PW"
 
-echo $REGISTRY_PW | docker login --username $CONTAINER_REGISTRY_UN --password-stdin $CONTAINER_REGISTRY
+echo $CONTAINER_REGISTRY_PW | docker login --username $CONTAINER_REGISTRY_UN --password-stdin $CONTAINER_REGISTRY
 docker tag ghcr.io/jentezar/arithmetic-service:$VERSION $CONTAINER_REGISTRY/arithmetic-service:$VERSION
 docker push $CONTAINER_REGISTRY/arithmetic-service:$VERSION
